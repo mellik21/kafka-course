f269094a714fffc6aeb0ee22b9589b309c678cf7
Anastasiia Solomakhina <anastasiia_solomakhina@epam.com>
2022-03-04 21:17:23 +0400

    Added synchronous producer with keys

---
+++ src/main/java/com/github/mellik21/simple_example/ProducerDemoKeys.java
@@ -0,0 +1,58 @@
+package com.github.mellik21.simple_example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+public class ProducerDemoKeys {
+    private static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
+    private static final String TOPIC_NAME = "example_topic";
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        KafkaProducer<String, String> producer = createProducer();
+
+        for (int i = 0; i < 10; i++) {
+            String value = "hello world #" + i;
+            String key = "id_" + i;
+
+            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, key, value);
+            log.info("Let's start sending record with Key=" + key);
+
+            //-asynchronous
+            producer.send(record, getCallbackAndLog())
+                    .get(); //block the .send to make it synchronous
+        }
+        producer.flush();
+        producer.close();
+    }
+
+    private static KafkaProducer<String, String> createProducer() {
+        //create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create the producer
+        return new KafkaProducer<>(properties);
+    }
+
+    private static Callback getCallbackAndLog() {
+        return (recordMetadata, e) -> {
+            if (e == null) {
+                log.info("Recieved new metadata \n" +
+                        "Topic:" + recordMetadata.topic() + "\n" +
+                        "Partition:" + recordMetadata.partition() + "\n" +
+                        "Offset:" + recordMetadata.offset() + "\n" +
+                        "Timestamp:" + recordMetadata.timestamp());
+            } else {
+                log.error("Error while producing", e.getMessage());
+            }
+        };
+    }
+
+}
